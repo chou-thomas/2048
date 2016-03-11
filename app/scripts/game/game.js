63545c2da7466129356118b1bd1d1cb03f19d6a7
chou.thomas <chou.thomas3@gmail.com>
2016-03-10 22:24:02 -0800

    tdd initial files

---
+++ app/scripts/game/game.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('Game',[]);
+.service('GameManager', function() {
+	//create a new game
+	this.newGame = function() {};
+	// Handle the move action
+	this.move = function() {};
+	//update the score
+	this.updateScore = function(newScore) {};
+	// Moves Available
+	this.movesAvailable = function() {};
+})
